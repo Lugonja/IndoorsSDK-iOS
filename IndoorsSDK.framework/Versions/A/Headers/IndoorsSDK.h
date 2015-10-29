053c3b8e927443ff5953e9c62d20152bd43509a3
Aleksandar Lugonja <aleksandar@indoo.rs>
2015-10-29 12:03:57 +0100

    Missing umbrella header added.

---
+++ IndoorsSDK.framework/Versions/A/Headers/IndoorsSDK.h
@@ -0,0 +1,27 @@
+//
+//  IndoorsSDK.h
+//  IndoorsSDK
+//
+//  Created by indoo.rs on 21/10/15.
+//  Copyright © 2015 Indoors. All rights reserved.
+//
+
+#ifndef IndoorsSDK_h
+#define IndoorsSDK_h
+
+#import <IndoorsSDK/Indoors.h>
+
+#import <IndoorsSDK/IndoorsLocationListener.h>
+#import <IndoorsSDK/ISPointAnnotationView.h>
+#import <IndoorsSDK/IDSCoordinate.h>
+#import <IndoorsSDK/IndoorsDelegate.h>
+#import <IndoorsSDK/IDSServiceAuthentication.h>
+#import <IndoorsSDK/IndoorsError.h>
+#import <IndoorsSDK/IDSBuilding.h>
+#import <IndoorsSDK/IDSZone.h>
+#import <IndoorsSDK/IDSZonePoint.h>
+#import <IndoorsSDK/IDSFloor.h>
+#import <IndoorsSDK/IDSDebugStream.h>
+#import <IndoorsSDK/ISIndoorsSurfaceViewController.h>
+
+#endif /* IndoorsSDK_h */
